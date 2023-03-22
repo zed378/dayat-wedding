b98b1ad50a4af3091d80194a164c0de4f462dc90
zed378 <kembedt@gmail.com>
2023-03-22 22:52:59 +0700

    add auto music player

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      use: [
+        {
+          loader: "url-loader",
+          options: {
+            name: "[name]-[hash].[ext]",
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,29 +5,34 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 // components
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+
+// assets
 import tr from "../../public/tr.webp";
+import music from "../../public/audio-file.mp3";
 
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
   if (completed) {
     // Render a completed state
     return (
-      <div className="w-full">
+      <div className="w-full h-screen flex flex-col justify-center items-center">
         <div className="w-full p-2 grid gap-2 md:grid-cols-4 grid-cols-2">
-          <div className="bg-pink-500 text-white flex flex-col justify-center items-center h-[100px]">
-            <h1 className="text-4xl">{days}</h1>
-            <h2>Days</h2>
+          <div className="bg-pink-500 text-white flex flex-col justify-center items-center h-[100px] md:h-[200px]">
+            <h1 className="text-4xl md:text-[4rem] mb-2">{days}</h1>
+            <h2 className="md:text-4xl">Hari</h2>
           </div>
-          <div className="bg-pink-500 text-white flex flex-col justify-center items-center h-[100px]">
-            <h1 className="text-4xl">{hours}</h1>
-            <h2>Hours</h2>
+          <div className="bg-pink-600 text-white flex flex-col justify-center items-center h-[100px] md:h-[200px]">
+            <h1 className="text-4xl md:text-[4rem] mb-2">{hours}</h1>
+            <h2 className="md:text-4xl">Jam</h2>
           </div>
-          <div className="bg-pink-500 text-white flex flex-col justify-center items-center h-[100px]">
-            <h1 className="text-4xl">{minutes}</h1>
-            <h2>Minutes</h2>
+          <div className="bg-pink-600 md:bg-pink-500 text-white flex flex-col justify-center items-center h-[100px] md:h-[200px]">
+            <h1 className="text-4xl md:text-[4rem] mb-2">{minutes}</h1>
+            <h2 className="md:text-4xl">Menit</h2>
           </div>
-          <div className="bg-pink-500 text-white flex flex-col justify-center items-center h-[100px]">
-            <h1 className="text-4xl">{seconds}</h1>
-            <h2>Seconds</h2>
+          <div className="bg-pink-500 md:bg-pink-600 text-white flex flex-col justify-center items-center h-[100px] md:h-[200px]">
+            <h1 className="text-4xl md:text-[4rem] mb-2">{seconds}</h1>
+            <h2 className="md:text-4xl">Detik</h2>
           </div>
         </div>
         <div className="w-full px-2 py-3 bg-pink-500 text-center text-4xl text-white mt-2">
@@ -41,31 +46,37 @@ const renderer = ({ days, hours, minutes, seconds, completed }) => {
       <div className="w-full h-screen">
         <div className="w-full h-screen relative">
           <div className="flex justify-end">
-            <Image src={tr} width={250} height={250} alt="garnish" />
+            <Image
+              src={tr}
+              className="h-[250px] w-[250px] md:h-[350px] md:w-[350px]"
+              alt="garnish"
+            />
           </div>
 
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full px-3 z-10">
-            <div className="text-center text-pink-500 mb-3">
-              <h1>Akad nikah akan dilaksanakan pada:</h1>
-              <h1 className="text-2xl">Jumat, 12 Mei 2023</h1>
+            <div className="text-center text-pink-500 mb-6">
+              <h1 className="md:text-4xl mb-2">
+                Akad nikah akan dilaksanakan pada:
+              </h1>
+              <h1 className="text-2xl md:text-[3rem]">Jumat, 12 Mei 2023</h1>
             </div>
 
             <div className="w-full p-2 grid gap-2 md:grid-cols-4 grid-cols-2">
-              <div className="bg-pink-500 text-white flex flex-col justify-center items-center h-[100px]">
-                <h1 className="text-4xl">{days}</h1>
-                <h2>Days</h2>
+              <div className="bg-pink-500 text-white flex flex-col justify-center items-center h-[100px] md:h-[200px]">
+                <h1 className="text-4xl md:text-[4rem] mb-2">{days}</h1>
+                <h2 className="md:text-4xl">Hari</h2>
               </div>
-              <div className="bg-pink-500 text-white flex flex-col justify-center items-center h-[100px]">
-                <h1 className="text-4xl">{hours}</h1>
-                <h2>Hours</h2>
+              <div className="bg-pink-600 text-white flex flex-col justify-center items-center h-[100px] md:h-[200px]">
+                <h1 className="text-4xl md:text-[4rem] mb-2">{hours}</h1>
+                <h2 className="md:text-4xl">Jam</h2>
               </div>
-              <div className="bg-pink-500 text-white flex flex-col justify-center items-center h-[100px]">
-                <h1 className="text-4xl">{minutes}</h1>
-                <h2>Minutes</h2>
+              <div className="bg-pink-600 md:bg-pink-500 text-white flex flex-col justify-center items-center h-[100px] md:h-[200px]">
+                <h1 className="text-4xl md:text-[4rem] mb-2">{minutes}</h1>
+                <h2 className="md:text-4xl">Menit</h2>
               </div>
-              <div className="bg-pink-500 text-white flex flex-col justify-center items-center h-[100px]">
-                <h1 className="text-4xl">{seconds}</h1>
-                <h2>Seconds</h2>
+              <div className="bg-pink-500 md:bg-pink-600 text-white flex flex-col justify-center items-center h-[100px] md:h-[200px]">
+                <h1 className="text-4xl md:text-[4rem] mb-2">{seconds}</h1>
+                <h2 className="md:text-4xl">Detik</h2>
               </div>
             </div>
           </div>
@@ -73,10 +84,8 @@ const renderer = ({ days, hours, minutes, seconds, completed }) => {
           <div className="flex justify-start absolute bottom-0">
             <Image
               src={tr}
-              width={250}
-              height={250}
+              className="h-[250px] w-[250px] md:h-[350px] md:w-[350px] rotate-180"
               alt="garnish"
-              className="rotate-180"
             />
           </div>
         </div>
@@ -89,6 +98,7 @@ export default function Home() {
   const d = new Date("May 12, 2023 09:00:00");
   const name = useRouter();
   const [open, setOpen] = useState(true);
+  const [play, setPlay] = useState(false);
 
   return (
     <>
@@ -97,6 +107,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
+
+      <div className="hidden">
+        <AudioPlayer src={music} autoPlay loop />
+      </div>
 
       {open && (
         <div className="bg-black w-full h-screen opening flex flex-col justify-center items-center gap-2">
@@ -122,13 +136,15 @@ export default function Home() {
           </div>
           <button
             className="bg-pink-600 px-6 py-3 rounded-xl text-white text-xl font-bold hover:bg-pink-800 mt-6"
-            onClick={() => setOpen(false)}
+            onClick={() => {
+              setOpen(false);
+              setPlay(true);
+            }}
           >
             Buka Undangan
           </button>
         </div>
       )}
-
       {!open && (
         <div className="w-full">
           <Countdown date={d} renderer={renderer} />

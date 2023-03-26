dc5f75cd1497e1619782f3282ed237e39813286e
zed378 <kembedt@gmail.com>
2023-03-26 16:58:21 +0700

    add new style

--- src/components/organisms/brideGroomsScreen.jsx
+++ src/components/organisms/brideGroomsScreen.jsx
@@ -2,11 +2,17 @@ import Image from "next/image";
 import groom from "../../../public/groom.svg";
 import bride from "../../../public/bride.svg";
 import love from "../../../public/love1.svg";
+import top from "../../../public/top.svg";
 
 export default function BrideGroomScreen() {
   return (
     <div className="w-full h-screen px-3 py-10 md:px-[15%] relative z-10">
       <div className="w-full h-full rounded-lg flex flex-col items-center justify-center bg-[#ffffff70] md:px-10 gap-2 relative z-10">
+        <Image
+          src={top}
+          alt="top"
+          className="w-[150px] md:w-[300px] mb-10 h-auto"
+        />
         <h1 className="text-center text-pink-500 text-3xl md:text-5xl mb-5 satisfy">
           The Wedding Of
         </h1>
@@ -29,41 +35,53 @@ export default function BrideGroomScreen() {
           />
         </div>
 
-        <div className="w-[90%] mt-5">
-          <h1 className="text-center text-pink-500 text-3xl font-light satisfy">
-            Moch. Rochmat Hidayat
-          </h1>
-          <p className="text-center font-light">
-            Putra Bapak Mugini dan Ibu Fitriah
-          </p>
-
-          <div className="flex gap-1 justify-center py-2">
-            <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
-            <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
-            <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
-            <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
-            <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
-            <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+        <div className="w-[90%] mt-5 md:flex md:justify-around md:items-center">
+          <div className="md:w-[40%] ">
+            <h1 className="text-center text-pink-500 text-3xl font-light satisfy">
+              Moch. Rochmat Hidayat
+            </h1>
+            <p className="text-center font-light">
+              Putra Bapak Mugini dan Ibu Fitriah
+            </p>
           </div>
 
-          <h1 className="text-center text-pink-500 text-3xl satisfy">dan</h1>
+          <div>
+            <div className="flex gap-1 justify-center py-2 ">
+              <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+              <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+              <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+              <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+              <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+              <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+            </div>
 
-          <div className="flex gap-1 justify-center py-2">
-            <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
-            <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
-            <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
-            <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
-            <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
-            <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+            <h1 className="text-center text-pink-500 text-3xl satisfy">dan</h1>
+
+            <div className="flex gap-1 justify-center py-2 ">
+              <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+              <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+              <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+              <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+              <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+              <div className="animate-pulse bg-pink-500 h-2 w-2 md:h-3 md:w-3 rounded-full"></div>
+            </div>
           </div>
 
-          <h1 className="text-center text-pink-500 text-3xl font-light satisfy">
-            Lubena
-          </h1>
-          <p className="text-center font-light">
-            Putra Alm. Bapak ... dan Ibu ...
-          </p>
+          <div className="md:w-[40%] ">
+            <h1 className="text-center text-pink-500 text-3xl font-light satisfy">
+              Lubena
+            </h1>
+            <p className="text-center font-light">
+              Putra Alm. Bapak ... dan Ibu ...
+            </p>
+          </div>
         </div>
+
+        <Image
+          src={top}
+          alt="top"
+          className="w-[150px] md:w-[300px] mt-10 h-auto rotate-180"
+        />
       </div>
     </div>
   );
